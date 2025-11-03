e1f21263aef0fb3a82ad047afe7cdd6d182a9783
Hentrixs <tobiasfala@gmail.com>
2025-11-03 17:31:25 -0300

    tarea3

--- script.js
+++ script.js
@@ -1,20 +1,23 @@
 
 let operacion = prompt('Dame una operacion (+,-,/,*,MD,IVA) ');
-
+// las const se declaran al principio del programa
 const SUMA = '+';
 const MUL = '*';
 const DIV = '/';
 const REST = '-';
 const MD = 'MD';
 const IVA = 'IVA';
+// las abreviaciones son problematicas en especial si no estan en contexto
+// no abreviar las constantes y en general eviten abreviar
 
 if (operacion !== SUMA && operacion !== MUL && operacion !== DIV && operacion !== REST && operacion !== MD && operacion !== IVA) {
     alert("Operacion incorrecta");
 } else {
 
-    if (operacion !== MD && operacion !== IVA) {
-
+    if (operacion !== MD && operacion !== IVA) { 
+// en el futuro existiran 300 operaciones 
         let a = prompt("Dame un numero");
+        // en el UI es mejor pedir y validar de un valor a otro porque si no le arruinas la UI
         let b = prompt("Dame otro numero");
 
         while (!a || isNaN(a)) {
@@ -39,13 +42,15 @@ if (operacion !== SUMA && operacion !== MUL && operacion !== DIV && operacion !=
     } else if (operacion == MD) {
         
         let cantidad = prompt("Escribe en numero la cantidad de numeros a promediar");
+        // TODO= Falta validacion de cantidad
         Number(cantidad);
 
-        let acumulador = 0;
-        Number(acumulador);
-        for (x = 0; x < cantidad; x++) {
-            let y = prompt(`Inserte numero ${x + 1}`);
-            acumulador += Number(y);
+
+        let acumulador = 0; //nota, usar nombres con i para el for (i, iterador, iterator , etc)
+        for (x = 0; x < cantidad; x++) { //ese let x es local, este bloque de codigo
+            let y = prompt(`Inserte numero ${x + 1}`);// y este bloq de codigo es donde ocurre la rep del bucle
+            acumulador += Number(y); // el y cambiarlo por nombres mas especifico
+            // TODO = valida numero
         };
 
         alert(acumulador / cantidad);
